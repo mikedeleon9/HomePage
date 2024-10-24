df8664c8b3adef18679464845f62059ad6fcee84
mikedeleon9 <mikedeleon9@gmail.com>
2024-10-24 16:53:46 -0400

    adding images and titles

--- myhomepage/src/components/mainSection.js
+++ myhomepage/src/components/mainSection.js
@@ -9,13 +9,13 @@ import immaculate from "../Images/Immaculate.png";
 export default function Main(){
     return(
         <section>
-        <div class="middle container border">
-            <div class="w-1/2 relative grid grid-cols-3 p-12 gap-6">
-                <div class="site"><img class="photo w-12" src={connections} alt="connections"/></div>
-                <div class="site"><img class="photo w-12" src={wordle} alt="wordle"/></div>
-                <div class="site"><img class="photo w-12" src={sudoku} alt="sudoku"/></div>
-               <div class="site"><img class="photo w-12" src={crossword} alt="crossword"/></div>
-                <div class="site"><img class="photo w-12" src={immaculate} alt="immaculate"/></div>
+        <div class="middle container ">
+            <div class="w-1/2 relative grid grid-cols-2 p-12 gap-6 border-4">
+                <div class="site"><img class="photo w-12" src={connections} alt="connections"/>NYT Connections</div>
+                <div class="site"><img class="photo w-12" src={wordle} alt="wordle"/>Wordle</div>
+                <div class="site"><img class="photo w-12" src={sudoku} alt="sudoku"/>Daily Killer Sudoku</div>
+               <div class="site"><img class="photo w-12" src={crossword} alt="crossword"/>NYT Crossword Puzzle</div>
+                <div class="site"><img class="photo w-12" src={immaculate} alt="immaculate"/>Immaculate Grid</div>
             </div>
         </div>
     </section>
