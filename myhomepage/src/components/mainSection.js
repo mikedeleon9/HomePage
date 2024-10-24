92e7dbbf962b49f8771934fc75c411d8cfcabf54
mikedeleon9 <mikedeleon9@gmail.com>
2024-10-24 17:05:43 -0400

    adding links

--- myhomepage/src/components/mainSection.js
+++ myhomepage/src/components/mainSection.js
@@ -11,11 +11,11 @@ export default function Main(){
         <section>
         <div class="middle container ">
             <div class="w-1/2 relative grid grid-cols-2 p-12 gap-6 border-4">
-                <div class="site"><img class="photo w-12" src={connections} alt="connections"/>NYT Connections</div>
-                <div class="site"><img class="photo w-12" src={wordle} alt="wordle"/>Wordle</div>
-                <div class="site"><img class="photo w-12" src={sudoku} alt="sudoku"/>Daily Killer Sudoku</div>
-               <div class="site"><img class="photo w-12" src={crossword} alt="crossword"/>NYT Crossword Puzzle</div>
-                <div class="site"><img class="photo w-12" src={immaculate} alt="immaculate"/>Immaculate Grid</div>
+            <a href="https://www.nytimes.com/games/connections" target="_blank"><div class="site"><img class="photo w-12" src={connections} alt="connections"/>NYT Connections</div></a>
+            <a href="https://www.nytimes.com/games/wordle/index.html" target="_blank"><div class="site"><img class="photo w-12" src={wordle} alt="wordle"/>Wordle</div></a>
+            <a href="https://www.dailykillersudoku.com/" target="_blank"><div class="site"><img class="photo w-12" src={sudoku} alt="sudoku"/>Daily Killer Sudoku</div></a>
+            <a href="https://www.nytimes.com/crosswords" target="_blank"><div class="site"><img class="photo w-12" src={crossword} alt="crossword"/>NYT Crossword Puzzle</div></a>
+            <a href="https://www.immaculategrid.com/" target="_blank"><div class="site"><img class="photo w-12" src={immaculate} alt="immaculate"/>Immaculate Grid</div></a>
             </div>
         </div>
     </section>
