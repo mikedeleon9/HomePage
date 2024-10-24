871b07d37aa3e8cdaa5e1a90e77972146d5a5b55
mikedeleon9 <mikedeleon9@gmail.com>
2024-10-24 15:54:07 -0400

    adding more styling

--- myhomepage/src/components/mainSection.js
+++ myhomepage/src/components/mainSection.js
@@ -5,16 +5,17 @@ import sudoku from "../Images/Sudoku.png";
 import crossword from "../Images/Crossword.png";
 import immaculate from "../Images/Immaculate.png";
 
+
 export default function Main(){
     return(
         <section>
-        <div class="middle container">
-            <div class="game-list">
-                <div class="site"><img class="photo" src={connections} alt="connections"/></div>
-                <div class="site"><img class="photo" src={wordle} alt="wordle"/></div>
-                <div class="site"><img class="photo" src={sudoku} alt="sudoku"/></div>
-               <div class="site"><img class="photo" src={crossword} alt="crossword"/></div>
-                <div class="site"><img class="photo" src={immaculate} alt="immaculate"/></div>
+        <div class="middle container ">
+            <div class="w-1/2 relative grid grid-cols-3 p-12 gap-6 border border-2">
+               <img class="photo w-12" src={connections} alt="connections"/>
+               <img class="photo w-12" src={wordle} alt="wordle"/>
+               <img class="photo w-12" src={sudoku} alt="sudoku"/>
+               <img class="photo w-12" src={crossword} alt="crossword"/>
+               <img class="photo w-12" src={immaculate} alt="immaculate"/>
             </div>
         </div>
     </section>
