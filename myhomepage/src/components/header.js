b47584cf753544d4d79768ca71250682c0f6a780
mikedeleon9 <mikedeleon9@gmail.com>
2024-10-25 14:43:07 -0400

    still going trying to add responsiveness

--- myhomepage/src/components/header.js
+++ myhomepage/src/components/header.js
@@ -4,8 +4,8 @@ export default function Header(){
     return(
        
     <header>
-        <div class="flex justify-between px-24 py-8 align-center bg-customBlue border-b-8 border-customOrange text-white">
-        <h1 class="text-2xl"><span class="text-customOrange">My</span> Homepage</h1>
+        <div class="flex justify-between px-24 py-8 align-center bg-customBlue border-b-8 border-customOrange text-white ">
+        <h1 class="text-2xl md:text-md"><span class="text-customOrange">My</span> Homepage</h1>
         <nav>
             <ul class="links flex gap-4 cursor-pointer">
                 <span class="text-customOrange"><li>Home</li></span>
--- myhomepage/src/components/mainSection.js
+++ myhomepage/src/components/mainSection.js
@@ -8,9 +8,9 @@ import immaculate from "../Images/Immaculate.png";
 
 export default function Main(){
     return(
-    <section class="flex justify-center">
-        <div class="middle container flex justify-center grid grid-cols-2 border-2 ">
-            <div class="relative grid grid-cols-3 p-12 gap-6 border-4">
+    <section class="flex justify-center p-4">
+        <div class="middle container flex justify-center grid grid-cols-2 ">
+            <div class="relative grid grid-cols-3 p-12 gap-6 max-sm:grid-cols-1 border-2 max-lg:grid-cols-3 max-xl:grid-cols-4">
             <a href="https://www.nytimes.com/games/connections" target="_blank"><div class="site"><img class="photo w-12 " src={connections} alt="connections"/>NYT Connections</div></a>
             <a href="https://www.nytimes.com/games/wordle/index.html" target="_blank"><div class="site"><img class="photo w-12" src={wordle} alt="wordle"/>Wordle</div></a>
             <a href="https://www.dailykillersudoku.com/" target="_blank"><div class="site"><img class="photo w-12" src={sudoku} alt="sudoku"/>Daily Killer Sudoku</div></a>
@@ -18,13 +18,13 @@ export default function Main(){
             <a href="https://www.immaculategrid.com/" target="_blank"><div class="site"><img class="photo w-12" src={immaculate} alt="immaculate"/>Immaculate Grid</div></a>
             </div>
 
-       {/*     <div class="relative grid grid-cols-3 p-12 gap-6 border-4">
-        <a href="https://www.nytimes.com/games/connections" target="_blank"><div class="site"><img class="photo w-12" src={connections} alt="connections"/>NYT Connections</div></a>
+           <div class="relative grid grid-cols-3 p-12 gap-6 border-2 max-sm:grid-cols-1 border-2 max-lg:grid-cols-3 max-xl:grid-cols-4">
+        <a href="https://www.nytimes.com/games/connections" target="_blank"><div class="site"><img class="w-8 sm:w-10 md:w-12" src={connections} alt="connections"/>NYT Connections</div></a>
             <a href="https://www.nytimes.com/games/wordle/index.html" target="_blank"><div class="site"><img class="photo w-12" src={wordle} alt="wordle"/>Wordle</div></a>
             <a href="https://www.dailykillersudoku.com/" target="_blank"><div class="site"><img class="photo w-12" src={sudoku} alt="sudoku"/>Daily Killer Sudoku</div></a>
             <a href="https://www.nytimes.com/crosswords" target="_blank"><div class="site"><img class="photo w-12" src={crossword} alt="crossword"/>NYT Crossword Puzzle</div></a>
             <a href="https://www.immaculategrid.com/" target="_blank"><div class="site"><img class="photo w-12" src={immaculate} alt="immaculate"/>Immaculate Grid</div></a>
-        </div> */}
+        </div> 
         </div>
 
        
