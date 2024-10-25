76906b67df144481dec50781f8d5aa0c8d7bd05e
mikedeleon9 <mikedeleon9@gmail.com>
2024-10-25 12:32:56 -0400

    trying to add responsiveness

--- myhomepage/src/components/mainSection.js
+++ myhomepage/src/components/mainSection.js
@@ -8,16 +8,26 @@ import immaculate from "../Images/Immaculate.png";
 
 export default function Main(){
     return(
-        <section>
-        <div class="middle container ">
-            <div class="w-1/2 relative grid grid-cols-3 p-12 gap-6 border-4">
-            <a href="https://www.nytimes.com/games/connections" target="_blank"><div class="site"><img class="photo w-12" src={connections} alt="connections"/>NYT Connections</div></a>
+    <section class="flex justify-center">
+        <div class="middle container flex justify-center grid grid-cols-2 border-2 ">
+            <div class="relative grid grid-cols-3 p-12 gap-6 border-4">
+            <a href="https://www.nytimes.com/games/connections" target="_blank"><div class="site"><img class="photo w-12 " src={connections} alt="connections"/>NYT Connections</div></a>
             <a href="https://www.nytimes.com/games/wordle/index.html" target="_blank"><div class="site"><img class="photo w-12" src={wordle} alt="wordle"/>Wordle</div></a>
             <a href="https://www.dailykillersudoku.com/" target="_blank"><div class="site"><img class="photo w-12" src={sudoku} alt="sudoku"/>Daily Killer Sudoku</div></a>
             <a href="https://www.nytimes.com/crosswords" target="_blank"><div class="site"><img class="photo w-12" src={crossword} alt="crossword"/>NYT Crossword Puzzle</div></a>
             <a href="https://www.immaculategrid.com/" target="_blank"><div class="site"><img class="photo w-12" src={immaculate} alt="immaculate"/>Immaculate Grid</div></a>
             </div>
+
+       {/*     <div class="relative grid grid-cols-3 p-12 gap-6 border-4">
+        <a href="https://www.nytimes.com/games/connections" target="_blank"><div class="site"><img class="photo w-12" src={connections} alt="connections"/>NYT Connections</div></a>
+            <a href="https://www.nytimes.com/games/wordle/index.html" target="_blank"><div class="site"><img class="photo w-12" src={wordle} alt="wordle"/>Wordle</div></a>
+            <a href="https://www.dailykillersudoku.com/" target="_blank"><div class="site"><img class="photo w-12" src={sudoku} alt="sudoku"/>Daily Killer Sudoku</div></a>
+            <a href="https://www.nytimes.com/crosswords" target="_blank"><div class="site"><img class="photo w-12" src={crossword} alt="crossword"/>NYT Crossword Puzzle</div></a>
+            <a href="https://www.immaculategrid.com/" target="_blank"><div class="site"><img class="photo w-12" src={immaculate} alt="immaculate"/>Immaculate Grid</div></a>
+        </div> */}
         </div>
+
+       
     </section>
     )
 
