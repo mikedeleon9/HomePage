32c3ac30842b4fb026fb35b80c5fd99759fc7b22
mikedeleon9 <mikedeleon9@gmail.com>
2024-10-28 11:08:11 -0400

    adding coding sites to the page

--- myhomepage/src/components/mainSection.js
+++ myhomepage/src/components/mainSection.js
@@ -4,25 +4,27 @@ import wordle from "../Images/Wordle.png";
 import sudoku from "../Images/Sudoku.png";
 import crossword from "../Images/Crossword.png";
 import immaculate from "../Images/Immaculate.png";
-
+import CSSBattle from "../Images/CSSBattle.png";
+import Codepen from "../Images/Codepen.png";
+import Tailwind from "../Images/Tailwind.png"
 
 export default function Main(){
     return(
     <section class="flex justify-center p-4">
         <div class="middle container flex justify-center grid grid-cols-2 ">
-            <div class="relative grid grid-cols-3 p-12 gap-6 max-sm:grid-cols-1 border-2 max-lg:grid-cols-3 max-xl:grid-cols-4">
-            <a href="https://www.nytimes.com/games/connections" target="_blank"><div class="site"><img class="photo w-12 " src={connections} alt="connections"/>NYT Connections</div></a>
+            <div class="relative grid grid-cols-3 p-6 gap-6 max-sm:grid-cols-1 border-2 max-lg:grid-cols-3 max-xl:grid-cols-4">
+            <a href="https://www.nytimes.com/games/connections" target="_blank"><div class="site"><img class="photo w-12 " src={connections} alt="connections"/>Connections</div></a>
             <a href="https://www.nytimes.com/games/wordle/index.html" target="_blank"><div class="site"><img class="photo w-12" src={wordle} alt="wordle"/>Wordle</div></a>
             <a href="https://www.dailykillersudoku.com/" target="_blank"><div class="site"><img class="photo w-12" src={sudoku} alt="sudoku"/>Daily Killer Sudoku</div></a>
-            <a href="https://www.nytimes.com/crosswords" target="_blank"><div class="site"><img class="photo w-12" src={crossword} alt="crossword"/>NYT Crossword Puzzle</div></a>
+            <a href="https://www.nytimes.com/crosswords" target="_blank"><div class="site"><img class="photo w-12" src={crossword} alt="crossword"/>NYT Crossword</div></a>
             <a href="https://www.immaculategrid.com/" target="_blank"><div class="site"><img class="photo w-12" src={immaculate} alt="immaculate"/>Immaculate Grid</div></a>
             </div>
 
-           <div class="relative grid grid-cols-3 p-12 gap-6 border-2 max-sm:grid-cols-1 border-2 max-lg:grid-cols-3 max-xl:grid-cols-4">
-        <a href="https://www.nytimes.com/games/connections" target="_blank"><div class="site"><img class="w-8 sm:w-10 md:w-12" src={connections} alt="connections"/>NYT Connections</div></a>
-            <a href="https://www.nytimes.com/games/wordle/index.html" target="_blank"><div class="site"><img class="photo w-12" src={wordle} alt="wordle"/>Wordle</div></a>
-            <a href="https://www.dailykillersudoku.com/" target="_blank"><div class="site"><img class="photo w-12" src={sudoku} alt="sudoku"/>Daily Killer Sudoku</div></a>
-            <a href="https://www.nytimes.com/crosswords" target="_blank"><div class="site"><img class="photo w-12" src={crossword} alt="crossword"/>NYT Crossword Puzzle</div></a>
+           <div class="relative grid grid-cols-3 p-6 gap-6 border-2 max-sm:grid-cols-1 border-2 max-lg:grid-cols-3 max-xl:grid-cols-4">
+        <a href="https://cssbattle.dev/" target="_blank"><div class="site"><img class="photo w-12" src={CSSBattle} alt="CSS-Battle"/>CSS Battle</div></a>
+            <a href="https://codepen.io/your-work/" target="_blank"><div class="site"><img class="photo w-12" src={Codepen} alt="codepen"/>Codepen</div></a>
+            <a href="tailwindcss.com/" target="_blank"><div class="site"><img class="photo w-12" src={Tailwind} alt="tailwind"/>Tailwind CSS</div></a>
+            <a href="https://www.nytimes.com/crosswords" target="_blank"><div class="site"><img class="photo w-12" src={crossword} alt="crossword"/>NYT Crossword </div></a>
             <a href="https://www.immaculategrid.com/" target="_blank"><div class="site"><img class="photo w-12" src={immaculate} alt="immaculate"/>Immaculate Grid</div></a>
         </div> 
         </div>
