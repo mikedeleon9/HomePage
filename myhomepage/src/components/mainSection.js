da6283ac59c7cbacc4536e4e7772505498d273f7
mikedeleon9 <mikedeleon9@gmail.com>
2024-10-28 12:28:56 -0400

    finishing adding logos

--- myhomepage/src/components/mainSection.js
+++ myhomepage/src/components/mainSection.js
@@ -7,6 +7,9 @@ import immaculate from "../Images/Immaculate.png";
 import CSSBattle from "../Images/CSSBattle.png";
 import Codepen from "../Images/Codepen.png";
 import Tailwind from "../Images/Tailwind.png"
+import Github from "../Images/Github.png";
+import W3Schools from "../Images/w3Schools.png"
+import ChatGPT from "../Images/ChatGPT.png"
 
 export default function Main(){
     return(
@@ -23,9 +26,10 @@ export default function Main(){
            <div class="relative grid grid-cols-3 p-6 gap-6 border-2 max-sm:grid-cols-1 border-2 max-lg:grid-cols-3 max-xl:grid-cols-4">
         <a href="https://cssbattle.dev/" target="_blank"><div class="site"><img class="photo w-12" src={CSSBattle} alt="CSS-Battle"/>CSS Battle</div></a>
             <a href="https://codepen.io/your-work/" target="_blank"><div class="site"><img class="photo w-12" src={Codepen} alt="codepen"/>Codepen</div></a>
-            <a href="tailwindcss.com/" target="_blank"><div class="site"><img class="photo w-12" src={Tailwind} alt="tailwind"/>Tailwind CSS</div></a>
-            <a href="https://www.nytimes.com/crosswords" target="_blank"><div class="site"><img class="photo w-12" src={crossword} alt="crossword"/>NYT Crossword </div></a>
-            <a href="https://www.immaculategrid.com/" target="_blank"><div class="site"><img class="photo w-12" src={immaculate} alt="immaculate"/>Immaculate Grid</div></a>
+            <a href="https://tailwindcss.com/" target="_blank"><div class="site"><img class="photo w-12" src={Tailwind} alt="tailwind"/>Tailwind CSS</div></a>
+            <a href="https://github.com/mikedeleon9" target="_blank"><div class="site"><img class="photo w-12" src={Github} alt="Github"/>Github </div></a>
+            <a href="https://www.w3schools.com/" target="_blank"><div class="site"><img class="photo w-12" src={W3Schools} alt="W3Schools"/>W3 Schools</div></a>
+            <a href="https://chatgpt.com/" target="_blank"><div class="site"><img class="photo w-12" src={ChatGPT} alt="ChatGPT"/>ChatGPT </div></a>
         </div> 
         </div>
 
